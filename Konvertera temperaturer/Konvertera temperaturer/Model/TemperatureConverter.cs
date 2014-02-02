c802d372a71da24674d229dbdcbfb0cc94806b30
Kevin Fredriksson <kf222dh@student.lnu.se>
2014-02-02 01:33:05 +0100

    Börjat jobba på det och läst igenom

---
+++ Konvertera temperaturer/Konvertera temperaturer/Model/TemperatureConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Konvertera_temperaturer.Model
+{
+    public class TemperatureConverter
+    {
+    }
+}
