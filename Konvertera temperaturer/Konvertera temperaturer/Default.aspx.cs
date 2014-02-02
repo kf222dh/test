97f465f2544b7616befb71d12363183697aeba6b
Kevin Fredriksson <kf222dh@student.lnu.se>
2014-02-02 23:45:50 +0100

    Klar

--- Konvertera temperaturer/Konvertera temperaturer/Default.aspx.cs
+++ Konvertera temperaturer/Konvertera temperaturer/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Konvertera_temperaturer.Model;//Hämtar Konvertera_temperaturer klassen i modell
 
 namespace Konvertera_temperaturer
 {
@@ -12,6 +13,40 @@ namespace Konvertera_temperaturer
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ConvertButton_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                //Lagrar och gör om text till räkbar alltså int
+                var startValue = int.Parse(StartTextBox.Text);
+                var endValue = int.Parse(EndTextBox.Text);
+                var scaleValue = int.Parse(ScaleTextBox.Text);
+
+                //Kollar vad man har valt i mellan °C eller °F i tabellhuvudet.
+                if (!CelToFahRadioButton.Checked)
+                {
+                    First.Text = "&degF";
+                    Second.Text = "&degC";
+                }
+
+                for (int temp = startValue; temp <= endValue; temp += scaleValue)
+                {
+                    //Gör en tabell
+                    TableRow row = new TableRow();
+                    ConversionTable.Rows.Add(row);
+                    TableCell[] cells = new TableCell[] { new TableCell(), new TableCell() };
+                    row.Cells.AddRange(cells);
+
+                    cells[0].Text = temp.ToString();
+                    cells[1].Text = CelToFahRadioButton.Checked ?//Om den är checkad så
+                    TemperatureConverter.CelsiusToFahrenheit(temp).ToString() : TemperatureConverter.FahrenheitToCelsius(temp).ToString();//Konvertering(: betyder eller)
+                }
+
+                //Visar upp tabellen.
+                ConversionLabel.Visible = true;
+            }
         }
     }
 }
--- Konvertera temperaturer/Konvertera temperaturer/Model/TemperatureConverter.cs
+++ Konvertera temperaturer/Konvertera temperaturer/Model/TemperatureConverter.cs
@@ -7,5 +7,17 @@ namespace Konvertera_temperaturer.Model
 {
     public class TemperatureConverter
     {
+        //Gör om Celsius till Fahrenheit.
+        public static int CelsiusToFahrenheit(int degreesC)
+        {
+            return Convert.ToInt32(degreesC * 1.8 + 32);
+        }
+
+        //Gör om Fahrenheit till Celsius.
+        public static int FahrenheitToCelsius(int degreesF)
+        {
+            return Convert.ToInt32((degreesF - 32) * 5 / 9);
+        }
+
     }
 }
